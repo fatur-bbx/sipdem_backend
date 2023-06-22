5581568d4abd4750e2a8c99a514f12890b8f85c3
fatur-bbx <fatur21ti@mahasiswa.pcr.ac.id>
2023-06-23 05:26:41 +0700

    Penambahan migration - Tabel Donatur

---
+++ database/migrations/2023_06_22_222531_create_donatur_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donatur', function (Blueprint $table) {
+            $table->uuid('id_donatur')->primary();
+            $table->string('nama_donatur');
+            $table->string('alamat_email');
+            $table->string('telp_donatur');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donatur');
+    }
+};
