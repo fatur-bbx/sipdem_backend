697e8de32a46d703ff05b4367ac316266149ca2b
fatur-bbx <fatur21ti@mahasiswa.pcr.ac.id>
2023-06-23 05:43:21 +0700

    Penambahan Route API Admin

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::post('/', [AdminController::class, 'create']);
+    Route::get('/', [AdminController::class, 'index']);
+    Route::get('/{id}', [AdminController::class, 'show']);
+    Route::put('/{id}', [AdminController::class, 'update']);
+    Route::delete('/{id}', [AdminController::class, 'delete']);
+});
